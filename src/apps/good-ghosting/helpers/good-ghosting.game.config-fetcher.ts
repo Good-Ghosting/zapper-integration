4f73df21c3d551348c0c69bded7832011eb29ed6
meetkosiso <kosiso.gaxion@gmail.com>
2022-05-31 19:25:47 +0100

    feat: integration to zapper

---
+++ src/apps/good-ghosting/helpers/good-ghosting.game.config-fetcher.ts
@@ -0,0 +1,44 @@
+import { Inject, Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class GoodGhostingGameConfigFetcherHelper {
+  static baseURL = 'https://goodghosting-api.com/v1';
+
+  private async getGameConfigs() {
+    const url = `${GoodGhostingGameConfigFetcherHelper.baseURL}/games`;
+
+    const response = await axios.get<any>(url);
+    const gameConfigs = response.data;
+    const farms = [];
+
+    const gameContractAddresses = Object.keys(gameConfigs);
+
+    for (let i = 0; i < gameContractAddresses.length; i += 1) {
+      const gameContractAddress = gameContractAddresses[i];
+      const rewardTokenAddresses = [];
+
+      const { depositTokenAddress, rewardTokenAddress, id, contractVersion, incentiveTokenAddress } =
+        gameConfigs[gameContractAddress];
+
+      if (rewardTokenAddress) {
+        rewardTokenAddresses.push(rewardTokenAddress);
+      }
+
+      if (incentiveTokenAddress) {
+        rewardTokenAddresses.push(incentiveTokenAddress);
+      }
+
+      if (depositTokenAddress && contractVersion && id) {
+        farms.push({
+          address: id,
+          stakedTokenAddress: depositTokenAddress,
+          rewardTokenAddresses,
+          contractVersion,
+        });
+      }
+    }
+
+    return farms;
+  }
+}
